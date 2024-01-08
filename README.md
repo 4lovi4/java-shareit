758f8552f2bc49a99c2ec34dc5223c1664176d50
a.a.dyatlov <a.a.dyatlov@tinkoff.ru>
2024-01-09 02:18:14 +0300

    fix: исправление слияния с веткой main

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # java-shareit
+<<<<<<< HEAD
 alexey0dyatlov Shareit project implementation.
+=======
+Template repository for Shareit project.
+>>>>>>> 3126ddc (Initial commit)
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
-
 @Data
 @RequiredArgsConstructor
 public class User {
