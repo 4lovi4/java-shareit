23e27d0967bd285a5a7add212216b9beed5f36e7
Alexey Dyatlov <alexey.dyatlov@yandex.ru>
2024-01-19 02:07:54 +0300

    fix: docker-compose создание базы исправлены идентификаторы

---
+++ docker/create_shareit_db.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE shareit_db
+WITH
+    ENCODING = 'UTF-8'
+    OWNER = practicum
+    ALLOW_CONNECTIONS = true
+    CONNECTION LIMIT = -1;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS users (id bigint GENERATED ALWAYS PRIMARY KEY, name varchar(255),
+CREATE TABLE IF NOT EXISTS users (id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY, name varchar(255),
 email varchar(100) UNIQUE);
 
-CREATE TABLE IF NOT EXISTS items (id bigint GENERATED ALWAYS PRIMARY KEY, name varchar(255),
+CREATE TABLE IF NOT EXISTS items (id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY, name varchar(255),
 description varchar(1024), available boolean, owner_id bigint, request bigint,
 CONSTRAINT fk_items_to_user FOREIGN KEY(owner_id) REFERENCES users(id));
 
-CREATE TABLE IF NOT EXISTS bookings(id bigint GENERATED ALWAYS PRIMARY KEY, item_id bigint,
+CREATE TABLE IF NOT EXISTS bookings(id bigint GENERATED ALWAYS AS IDENTITY  PRIMARY KEY, item_id bigint,
 start_time timestamp, end_time timestamp, booker_id bigint, status varchar(16));
 
-CREATE TABLE IF NOT EXISTS requests(id bigint GENERATED ALWAYS PRIMARY KEY, description varchar(1024),
+CREATE TABLE IF NOT EXISTS requests(id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY, description varchar(1024),
 bigint requester_id, created_time timestamp);
